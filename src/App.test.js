4824865aa29db4f5c209929cc926501b453485d5
Prateek Varma <prtk.varma@gmail.com>
2022-08-05 12:21:20 -0600

    Wrote 2 passing tests, to find button and then to check it's correct values after it's clicked

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial values', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(colorButton).toHaveStyle({backgroundColor: "red"});
 });
+
+test("button turns blue when clicked, and button text changes to red", () => {
+  render(<App />);
+  //check if button exists
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  
+  //click the button
+  fireEvent.click(colorButton);
+
+  //expect the color to change to blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  //after the color has changed, expect the text to be "Change to red"
+  expect(colorButton.textContent).toBe("Change to red");
+})
